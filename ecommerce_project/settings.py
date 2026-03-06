750c7d9d83e96038c2ffde1d997a0ed67ad57744
Ritchelle <mrnainggie@tip.edu.ph>
2026-03-06 23:01:44 +0800

    Fix ALLOWED_HOSTS for new Render URL

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -34,9 +34,9 @@ SECRET_KEY = os.getenv('^h01bjr+%r4u)1+d=yd0zkw90-(4z))10q@1p7r1cgq3v0s62(', 'fa
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'ai-recommender-3-atn2.onrender.com',
-    '.onrender.com',
-    'localhost',
+    'ai-recommender-3-atn2.onrender.com', 
+    '.onrender.com', 
+    'localhost', 
     '127.0.0.1'
 ]
 
