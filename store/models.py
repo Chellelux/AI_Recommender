c85eeaa52bb362a08521473287c38ab7671ee5fd
Ritchelle <mrnainggie@tip.edu.ph>
2026-03-07 02:51:09 +0800

    Fix syntax

--- store/models.py
+++ store/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from djongo import models as djongo_models # Import Djongo models
+from djongo import models as djongo_models
 
 class Product(models.Model):
-    _id = djongo_models.ObjectIdField() # Use djongo_models here
+    _id = djongo_models.ObjectIdField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    tags = models.TextField(blank=True, null=True) 
+    tags = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
