9a0921024e1de0d80a1284fad4e7f1985cb35a59
Ritchelle <mrnainggie@tip.edu.ph>
2026-03-07 02:32:21 +0800

    Fix ObjectIdField import and add tags field

--- store/models.py
+++ store/models.py
@@ -1,13 +1,12 @@
-from djongo import models
 from django.db import models
-  
+from djongo import models as djongo_models # Import Djongo models
+
 class Product(models.Model):
-    _id = models.ObjectIdField()
-    name = models.CharField(max_length=100)
+    _id = djongo_models.ObjectIdField() # Use djongo_models here
+    name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.FloatField()
-    category = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     tags = models.TextField(blank=True, null=True) 
 
     def __str__(self):
-        return self.namepythp
+        return self.name
