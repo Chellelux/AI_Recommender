3f58ba88572e1dd633625079cc751794f21130bb
Ritchelle <mrnainggie@tip.edu.ph>
2026-03-07 22:30:40 +0800

    Fixing

--- store/apps.py
+++ store/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
-
+from mongoengine import connect
+import os
 
 class StoreConfig(AppConfig):
     name = 'store'
+
+    def ready(self):
+        # Only connect if not already connected
+        if not connect._connections:
+            mongo_uri = os.getenv("MONGO_URI")
+            connect(host=mongo_uri)
